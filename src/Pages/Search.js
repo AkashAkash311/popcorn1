5eedef2b941e0d1bf7f0c2f7ac25935ce3c2b284
Akash <73471799+AkashAkash311@users.noreply.github.com>
2024-06-26 19:26:36 +0530

    update Search.js

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -17,16 +17,18 @@ const Search = () => {
           SetResults({...response});
         }
         fetchBackgroundImg();
+        
 
-
-    },[Results] );
-    console.log(Results);
+    },[] );
+    // console.log(Results);
 
   return (
 
   
     <div className=' min-h-[1200px] h-fit max-w-[1200px]  mx-auto mt-16 text-white bg-black'>
-        <h2 className='text-[24px] text-white mb-6 pt-6 font-semibold'>Search results of '{state.movieName}' </h2>
+      <h2 className='text-[24px] text-white mb-6 pt-6 font-semibold'>Search results of '{state.movieName}' </h2>
+      { Results?.results?.length !== 0 ?
+
         <div className=' gap-x-6 gap-y-6 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 mb-3 '>
             {Results && Results.results.map((movie)=>(
             <div className='max-w-[230px] max-h-[402px] rounded-2xl cursor-pointer '>
@@ -34,9 +36,16 @@ const Search = () => {
             </div>
             ))}
         </div>
+      :
+        <div> Results not found</div>
+  }
     </div>
   )
 }
 
 export default Search
+
+// 3 != "3"
+
+
 
